ad6b7dc1c6bdca943473d5195280b42b17fb2b69
Ankit Singh1 <ankit_singh1@epam.com>
2022-05-03 17:41:34 +0530

    Leetcode 844 problem solutiion

---
+++ src/main/java/com/inn/problems/BackspaceStringCmp.java
@@ -0,0 +1,40 @@
+package com.inn.problems;
+
+public class BackspaceStringCmp {
+
+	public static void main(String[] args) {
+		System.out.println(backspaceCompare("a#c", "b"));
+	}
+
+	private static boolean backspaceCompare(String s, String t) {
+		String out1 = getStringValueAfterRemovingChar(s);
+		String out2 = getStringValueAfterRemovingChar(t);
+		return out1.equals(out2);
+	}
+	
+	private static String getStringValueAfterRemovingChar(String input) {
+		char[] charArray = new char[input.length()];
+		int pointer1 = 0;
+		for(char ch : input.toCharArray()) {
+			if(ch != '#') {
+				charArray[pointer1] = ch;
+				pointer1++;
+			}else {
+				if(pointer1 > 0) {
+					pointer1--;
+					charArray[pointer1] = '#';
+				}
+			}
+		}
+		
+		StringBuilder result = new StringBuilder();
+		for(char ch : charArray) {
+			if(Character.isAlphabetic(ch)) {
+				result.append(ch);
+			}
+		}
+		
+		return result.toString();
+	}
+
+}
